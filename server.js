ad4830ca8f0b827765b4f85d8cdf2bd336924f3b
David Situ <davidsitu626@gmail.com>
2019-02-01 08:40:17 -0800

    index route test complete get games route tests complete simple JS games array created

--- server.js
+++ server.js
@@ -4,10 +4,41 @@ const server = express();
 
 server.use(express.json());
 
+let games = [
+    {
+       title: 'Kingdom Hearts',
+       genre: 'RPG Fantasy',
+       releaseYear: '2002'
+    },
+    {
+        title: 'Kingdom Hearts 2',
+        genre: 'RPG Fantasy',
+        releaseYear: '2005'
+     },
+     {
+        title: 'Kingdom Hearts 3',
+        genre: 'RPG Fantasy',
+        releaseYear: '2019'
+     },
+     {
+        title: 'Kingdom Hearts 4',
+        genre: 'RPG Fantasy',
+        releaseYear: '2040'
+     }
+]
+
+
+
 server.get('/', (req, res) => {
     res
         .status(200)
         .json('running')
+})
+
+server.get('/games', (req, res) => {
+    res
+        .status(200)
+        .json(games)
 })
 
 module.exports = server
--- server.spec.js
+++ server.spec.js
@@ -11,5 +11,35 @@ describe('server.js', () => {
 
             expect(response.status).toBe(expectedStatusCode);
         })
+
+        it('should respond with json', async () => {
+            let response = await request(server).get('/');
+            expect(response.type).toMatch(/json/i)
+        })
+    })
+
+    describe('GET /games endpoint', () =>{
+        it('should return status 200', async () => {
+            const response = await request(server).get('/games');
+            expect(response.status).toBe(200)
+        })
+
+        it('should respond with json Object', async () => {
+            let response = await request(server).get('/games');
+            expect(response.type).toMatch(/json/i)
+        })
+
+        it('should send back object with title, genre, releaseYear as keys', async () => {
+            const expected =
+            {
+                title: 'Kingdom Hearts',
+                genre: 'RPG Fantasy',
+                releaseYear: '2002'
+             }
+
+             let response = await request(server).get('/games');
+             //.toBe references, toEqual equals the content
+             expect(response.body[0]).toEqual(expected);
+        })
     })
 })
